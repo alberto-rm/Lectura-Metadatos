97a2c48848e816dd20b4e8142552ea12abb47c6a
Albert <alberto.ramirez.m7@gmail.com>
2025-03-10 18:59:52 +0100

    Agregar archivos de proyecto.

---
+++ Lectura-Metadatos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lectura_Metadatos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
