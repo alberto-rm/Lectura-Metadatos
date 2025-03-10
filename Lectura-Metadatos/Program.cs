aa7253c6dbd0476233143a5a84ae294280f58c9c
Albert <alberto.ramirez.m7@gmail.com>
2025-03-10 19:24:02 +0100

    + Lectura de Bits de imagen y descodificar su valor

--- Lectura-Metadatos/Program.cs
+++ Lectura-Metadatos/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace Lectura_Metadatos
 {
@@ -6,7 +8,24 @@ namespace Lectura_Metadatos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string urlImage = @"C:\\Users\\AlbertPc\\source\\repos\\Lectura-Metadatos\\Lectura-Metadatos\\speaker.jpg";
+
+            Image image = new Bitmap(urlImage);
+
+            PropertyItem[] propiedades = image.PropertyItems;
+
+            System.Text.ASCIIEncoding encoding = new System.Text.ASCIIEncoding();
+
+            foreach (var propiedad in propiedades)
+            {
+                if (propiedad.Value != null)
+                {
+                    string valor = encoding.GetString(propiedad.Value);
+
+                    Console.WriteLine(valor);
+
+                }
+            }
         }
     }
 }
